9789bf3113ac6342421d8e4130adf0fd378ff314
silentmuh <silentmuh@163.com>
2019-04-07 23:57:12 +0800

    list页面分页显示

--- src/main/java/com/hegg/springboot/controller/UserThymeleafController.java
+++ src/main/java/com/hegg/springboot/controller/UserThymeleafController.java
@@ -1,17 +1,13 @@
 package com.hegg.springboot.controller;
 
+import com.github.pagehelper.PageInfo;
 import com.hegg.springboot.model.User;
 import com.hegg.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/userThymeleafController")
@@ -26,10 +22,11 @@ public class UserThymeleafController {
      * @return
      */
     @RequestMapping(value = "/list")
-    public ModelAndView list(Model model){  //这个地方用model来存储数据，里面存储键值对，值可以为list
-        List<User> userList = userService.getAllUser();
+    public ModelAndView list(Model model, @RequestParam(value = "pageNum") Integer pageNum,
+                             @RequestParam(value = "pageSize") Integer pageSize){  //这个地方用model来存储数据，里面存储键值对，值可以为list
+        PageInfo<User> findByPageInfo = userService.findByPageInfo(pageNum, pageSize);
         model.addAttribute("title","所有用户");
-        model.addAttribute("userList", userList);
+        model.addAttribute("findByPageInfo", findByPageInfo);
         return new ModelAndView("users/list", "userModel", model);
     }
 
